0d3e27c7e334a510675065c0946de63edd857988
Paden Pace <pjpace27@yahoo.com>
2017-06-24 22:03:45 -0500

    addition of Monk Collection features

--- assets/js/classCollections/monkCollections.js
+++ assets/js/classCollections/monkCollections.js
@@ -172,13 +172,13 @@
         features: [
           {
             name: "Unarmored Defense",
-            "desc": [
+            def: [
               "Beginning at 1st level, while you are wearing no armor and not wielding a shield, your AC equals 10 + your Dexterity modifier + your Wisdom modifier."
             ],
           },
           {
             name: "Martial Arts",
-            "desc": [
+            def: [
               "At 1st level, your practice of martial arts gives you mastery of combat styles that use unarmed strikes and monk weapons, which are shortswords and any simple melee weapons that don’t have the two- handed or heavy property.",
               "You gain the following benefits while you are unarmed or wielding only monk weapons and you aren’t wearing armor or wielding a shield:",
               "• You can use Dexterity instead of Strength for the attack and damage rolls of your unarmed strikes and monk weapons.",
@@ -209,7 +209,7 @@
         features: [
           {
             name: "Ki",
-            "desc": [
+            def: [
               "Starting at 2nd level, your training allows you to harness the mystic energy of ki. Your access to this energy is represented by a number of ki points. Your monk level determines the number of points you have, as shown in the Ki Points column of Table: The Monk.",
               "You can spend these points to fuel various ki features. You start knowing three such features: Flurry of Blows, Patient Defense, and Step of the Wind. You learn more ki features as you gain levels in this class.",
               "When you spend a ki point, it is unavailable until you finish a short or long rest, at the end of which you draw all of your expended ki back into yourself. You must spend at least 30 minutes of the rest meditating to regain your ki points.",
@@ -219,25 +219,25 @@
           },
           {
             name: "Flurry of Blows",
-            "desc": [
+            def: [
               "Immediately after you take the Attack action on your turn, you can spend 1 ki point to make two unarmed strikes as a bonus action."
             ],
           },
           {
             name: "Patient Defense",
-            "desc": [
+            def: [
               "You can spend 1 ki point to take the Dodge action as a bonus action on your turn."
             ],
           },
           {
             name: "Step of the Wind",
-            "desc": [
+            def: [
               "You can spend 1 ki point to take the Disengage or Dash action as a bonus action on your turn, and your jump distance is doubled for the turn."
             ],
           },
           {
             name: "Unarmored Movement 1",
-            "desc": [
+            def: [
               "Starting at 2nd level, your speed increases by 10 feet while you are not wearing armor or wielding a shield. This bonus increases when you reach certain monk levels, as shown in Table: The Monk.",
               "At 9th level, you gain the ability to move along vertical surfaces and across liquids on your turn without falling during the move."
             ],
@@ -264,13 +264,13 @@
         features: [
           {
             name: "Monastic Tradition",
-            "desc": [
+            def: [
               "When you reach 3rd level, you commit yourself to a monastic tradition: the Way of the Open Hand, the Way of Shadow, or the Way of the Four Elements, all detailed at the end of the class description. Your tradition grants you features at 3rd level and again at 6th, 11th, and 17th level."
             ],
           },
           {
             name: "Deflect Missiles",
-            "desc": [
+            def: [
               "Starting at 3rd level, you can use your reaction to deflect or catch the missile when you are hit by a ranged weapon attack. When you do so, the damage you take from the attack is reduced by 1d10 + your Dexterity modifier + your monk level.",
               "If you reduce the damage to 0, you can catch the missile if it is small enough for you to hold in one hand and you have at least one hand free. If you catch a missile in this way, you can spend 1 ki point to make a ranged attack with the weapon or piece of ammunition you just caught, as part of the same reaction. You make this attack with proficiency, regardless of your weapon proficiencies, and the missile counts as a monk weapon for the attack, which has a normal range of 20 feet and a long range of 60 feet."
             ],
@@ -297,13 +297,13 @@
         features: [
           {
             name: "Ability Score Improvement 1",
-            "desc": [
+            def: [
               "When you reach 4th level, and again at 8th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
             ],
           },
           {
             name: "Slow Fall",
-            "desc": [
+            def: [
               "Beginning at 4th level, you can use your reaction when you fall to reduce any falling damage you take by an amount equal to five times your monk level."
             ],
           }
@@ -329,13 +329,13 @@
         features: [
           {
             name: "Extra Attack",
-            "desc": [
+            def: [
               "Beginning at 5th level, you can attack twice, instead of once, whenever you take the Attack action on your turn."
             ],
           },
           {
             name: "Stunning Strike",
-            "desc": [
+            def: [
               "Starting at 5th level, you can interfere with the flow of ki in an opponent’s body. When you hit another creature with a melee weapon attack, you can spend 1 ki point to attempt a stunning strike. The target must succeed on a Constitution saving throw or be stunned until the end of your next turn."
             ],
           }
@@ -361,7 +361,7 @@
         features: [
           {
             name: "Ki Empowered Strikes",
-            "desc": [
+            def: [
               "Starting at 6th level, your unarmed strikes count as magical for the purpose of overcoming resistance and immunity to nonmagical attacks and damage."
             ],
           }
@@ -387,13 +387,13 @@
         features: [
           {
             name: "Evasion",
-            "desc": [
+            def: [
               "At 7th level, your instinctive agility lets you dodge out of the way of certain area effects, such as a blue dragon’s lightning breath or a fireball spell. When you are subjected to an effect that allows you to make a Dexterity saving throw to take only half damage, you instead take no damage if you succeed on the saving throw, and only half damage if you fail."
             ],
           },
           {
             name: "Stillness of Mind",
-            "desc": [
+            def: [
               "Starting at 7th level, you can use your action to end one effect on yourself that is causing you to be charmed or frightened."
             ],
           }
@@ -419,7 +419,7 @@
         features: [
           {
             name: "Ability Score Improvement 2",
-            "desc": [
+            def: [
               "When you reach 4th level, and again at 8th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
             ],
           }
@@ -445,7 +445,7 @@
         features: [
           {
             name: "Unarmored Movement 2",
-            "desc": [
+            def: [
               "Starting at 2nd level, your speed increases by 10 feet while you are not wearing armor or wielding a shield. This bonus increases when you reach certain monk levels, as shown in Table: The Monk.",
               "At 9th level, you gain the ability to move along vertical surfaces and across liquids on your turn without falling during the move."
             ],
@@ -472,7 +472,7 @@
         features: [
           {
             name: "Purity of Body",
-            "desc": [
+            def: [
               "At 10th level, your mastery of the ki flowing through you makes you immune to disease and poison."
             ],
           }
@@ -517,7 +517,7 @@
         features: [
           {
             name: "Ability Score Improvement 3",
-            "desc": [
+            def: [
               "When you reach 4th level, and again at 8th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
             ],
           }
@@ -543,7 +543,7 @@
         features: [
           {
             name: "Tongue of the Sun and Moon",
-            "desc": [
+            def: [
               "Starting at 13th level, you learn to touch the ki of other minds so that you understand all spoken languages. Moreover, any creature that can understand a language can understand what you say."
             ],
           }
@@ -569,7 +569,7 @@
         features: [
           {
             name: "Diamond Soul",
-            "desc": [
+            def: [
               "Beginning at 14th level, your mastery of ki grants you proficiency in all saving throws.",
               "Additionally, whenever you make a saving throw and fail, you can spend 1 ki point to reroll it and take the second result."
             ],
@@ -596,7 +596,7 @@
         features: [
           {
             name: "Timeless Body",
-            "desc": [
+            def: [
               "At 15th level, your ki sustains you so that you suffer none of the frailty of old age, and you can’t be aged magically. You can still die of old age, however. In addition, you no longer need food or water."
             ],
           }
@@ -622,7 +622,7 @@
         features: [
           {
             name: "Ability Score Improvement 4",
-            "desc": [
+            def: [
               "When you reach 4th level, and again at 8th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
             ],
           }
@@ -667,7 +667,7 @@
         features: [
           {
             name: "Empty Body",
-            "desc": [
+            def: [
               "Beginning at 18th level, you can use your action to spend 4 ki points to become invisible for 1 minute. During that time, you also have resistance to all damage but force damage.",
               "Additionally, you can spend 8 ki points to cast the astral projection spell, without needing material components. When you do so, you can’t take any other creatures with you."
             ],
@@ -694,7 +694,7 @@
         features: [
           {
             name: "Ability Score Improvement 5",
-            "desc": [
+            def: [
               "When you reach 4th level, and again at 8th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
             ],
           }
@@ -720,7 +720,7 @@
         features: [
           {
             name: "Perfect Self",
-            "desc": [
+            def: [
               "At 20th level, when you roll for initiative and have no ki points remaining, you regain 4 ki points."
             ],
           }
