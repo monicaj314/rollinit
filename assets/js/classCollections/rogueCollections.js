6ccf8b914a6bbe272960c109ada1001bf9d26152
Paden Pace <pjpace27@yahoo.com>
2017-06-24 22:45:31 -0500

    addition of Rogue  Collection features

--- assets/js/classCollections/rogueCollections.js
+++ assets/js/classCollections/rogueCollections.js
@@ -89,8 +89,7 @@
     ],
 
     featuresArray: [
-      {
-        _id: "58f40badc9e7ce9f721534c7",
+      { _id: "rogueLevel1",
         level: 1,
         ability_score_bonuses: 0,
         prof_bonus: 2,
@@ -101,10 +100,19 @@
         ],
         features: [
           {
-            name: "Sneak Attack"
+            name: "Sneak Attack",
+            def: [
+              "Beginning at 1st level, you know how to strike subtly and exploit a foe’s distraction. Once per turn, you can deal an extra 1d6 damage to one creature you hit with an attack if you have advantage on the attack roll. The attack must use a finesse or a ranged weapon.",
+              "You don’t need advantage on the attack roll if another enemy of the target is within 5 feet of it, that enemy isn’t incapacitated, and you don’t have disadvantage on the attack roll.",
+              "The amount of the extra damage increases as you gain levels in this class, as shown in the Sneak Attack column of the Rogue table."
+            ],
           },
           {
-            name: "Thieves' Cant"
+            name: "Thieves' Cant",
+            def: [
+              "During your rogue training you learned thieves’ cant, a secret mix of dialect, jargon, and code that allows you to hide messages in seemingly normal conversation. Only another creature that knows thieves’ cant understands such messages. It takes four times longer to convey such a message than it does to speak the same idea plainly.",
+              "In addition, you understand a set of secret signs and symbols used to convey short, simple messages, such as whether an area is dangerous or the territory of a thieves’ guild, whether loot is nearby, or whether the people in an area are easy marks or will provide a safe house for thieves on the run."
+            ],
           }
         ],
         class_specific: {
@@ -118,15 +126,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d1",
+      { _id: "rogueLevel2",
         level: 2,
         ability_score_bonuses: 0,
         prof_bonus: 2,
         feature_choices: [],
         features: [
           {
-            name: "Cunning Action"
+            name: "Cunning Action",
+            def: [
+              "Starting at 2nd level, your quick thinking and agility allow you to move and act quickly. You can take a bonus action on each of your turns in combat. This action can be used only to take the Dash, Disengage, or Hide action."
+            ],
           }
         ],
         class_specific: {
@@ -140,15 +150,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534cb",
+      { _id: "rogueLevel3",
         level: 3,
         ability_score_bonuses: 0,
         prof_bonus: 2,
         feature_choices: [],
         features: [
           {
-            name: "Roguish Archetype"
+            name: "Roguish Archetype",
+            def: [
+              "At 3rd level, you choose an archetype that you emulate in the exercise of your rogue abilities. The Thief archetype is detailed at the bottom of this page. Additional archetypes are available in the original source material. Your archetype choice grants you features at 3rd level and then again at 9th, 13th, and 17th level."
+            ],
           }
         ],
         class_specific: {
@@ -162,15 +174,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534c9",
+      { _id: "rogueLevel4",
         level: 4,
         ability_score_bonuses: 1,
         prof_bonus: 2,
         feature_choices: [],
         features: [
           {
-            name: "Ability Score Improvement 1"
+            name: "Ability Score Improvement 1",
+            def: [
+              "When you reach 4th level, and again at 8th, 10th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
+            ],
           }
         ],
         class_specific: {
@@ -184,15 +198,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534ca",
+      { _id: "rogueLevel5",
         level: 5,
         ability_score_bonuses: 1,
         prof_bonus: 3,
         feature_choices: [],
         features: [
           {
-            name: "Uncanny Dodge"
+            name: "Uncanny Dodge",
+            def: [
+              "Starting at 5th level, when an attacker that you can see hits you with an attack, you can use your reaction to halve the attack’s damage against you."
+            ],
           }
         ],
         class_specific: {
@@ -206,8 +222,7 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534cd",
+      { _id: "rogueLevel6",
         level: 6,
         ability_score_bonuses: 1,
         prof_bonus: 3,
@@ -228,15 +243,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534cc",
+      { _id: "rogueLevel7",
         level: 7,
         ability_score_bonuses: 1,
         prof_bonus: 3,
         feature_choices: [],
         features: [
           {
-            name: "Evasion"
+            name: "Evasion",
+            def: [
+              "Beginning at 7th level, you can nimbly dodge out of the way of certain area effects, such as a red dragon’s fiery breath or an ice storm spell. When you are subjected to an effect that allows you to make a Dexterity saving throw to take only half damage, you instead take no damage if you succeed on the saving throw, and only half damage if you fail."
+            ],
           }
         ],
         class_specific: {
@@ -250,15 +267,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534ce",
+      { _id: "rogueLevel8",
         level: 8,
         ability_score_bonuses: 2,
         prof_bonus: 3,
         feature_choices: [],
         features: [
           {
-            name: "Ability Score Improvement 2"
+            name: "Ability Score Improvement 2",
+            def: [
+              "When you reach 4th level, and again at 8th, 10th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
+            ],
           }
         ],
         class_specific: {
@@ -272,8 +291,7 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534cf",
+      { _id: "rogueLevel9",
         level: 9,
         ability_score_bonuses: 2,
         prof_bonus: 4,
@@ -294,15 +312,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d0",
+      { _id: "rogueLevel10",
         level: 10,
         ability_score_bonuses: 2,
         prof_bonus: 4,
         feature_choices: [],
         features: [
           {
-            name: "Ability Score Improvement 3"
+            name: "Ability Score Improvement 3",
+            def: [
+              "When you reach 4th level, and again at 8th, 10th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
+            ],
           }
         ],
         class_specific: {
@@ -316,15 +336,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d2",
+      { _id: "rogueLevel11",
         level: 11,
         ability_score_bonuses: 2,
         prof_bonus: 4,
         feature_choices: [],
         features: [
           {
-            name: "Reliable Talent"
+            name: "Reliable Talent",
+            def: [
+              "By 11th level, you have refined your chosen skills until they approach perfection. Whenever you make an ability check that lets you add your proficiency bonus, you can treat a d20 roll of 9 or lower as a 10."
+            ],
           }
         ],
         class_specific: {
@@ -338,15 +360,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d3",
+      { _id: "rogueLevel12",
         level: 12,
         ability_score_bonuses: 3,
         prof_bonus: 4,
         feature_choices: [],
         features: [
           {
-            name: "Ability Score Improvement 4"
+            name: "Ability Score Improvement 4",
+            def: [
+              "When you reach 4th level, and again at 8th, 10th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
+            ],
           }
         ],
         class_specific: {
@@ -360,8 +384,7 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d4",
+      { _id: "rogueLevel13",
         level: 13,
         ability_score_bonuses: 3,
         prof_bonus: 5,
@@ -378,15 +401,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d7",
+      { _id: "rogueLevel14",
         level: 14,
         ability_score_bonuses: 3,
         prof_bonus: 5,
         feature_choices: [],
         features: [
           {
-            name: "Blindsense"
+            name: "Blindsense",
+            def: [
+              "Starting at 14th level, if you are able to hear, you are aware of the location of any hidden or invisible creature within 10 feet of you."
+            ],
           }
         ],
         class_specific: {
@@ -400,15 +425,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d6",
+      { _id: "rogueLevel15",
         level: 15,
         ability_score_bonuses: 3,
         prof_bonus: 5,
         feature_choices: [],
         features: [
           {
-            name: "Slippery Mind"
+            name: "Slippery Mind",
+            def: [
+              "By 15th level, you have acquired greater mental strength. You gain proficiency in Wisdom saving throws."
+            ],
           }
         ],
         class_specific: {
@@ -422,15 +449,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d5",
+      { _id: "rogueLevel16",
         level: 16,
         ability_score_bonuses: 4,
         prof_bonus: 5,
         feature_choices: [],
         features: [
           {
-            name: "Ability Score Improvement 5"
+            name: "Ability Score Improvement 5",
+            def: [
+              "When you reach 4th level, and again at 8th, 10th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
+            ],
           }
         ],
         class_specific: {
@@ -444,8 +473,7 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d8",
+      { _id: "rogueLevel17",
         level: 17,
         ability_score_bonuses: 4,
         prof_bonus: 6,
@@ -462,15 +490,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534d9",
+      { _id: "rogueLevel18",
         level: 18,
         ability_score_bonuses: 4,
         prof_bonus: 6,
         feature_choices: [],
         features: [
           {
-            name: "Elusive"
+            name: "Elusive",
+            def: [
+              "Beginning at 18th level, you are so evasive that attackers rarely gain the upper hand against you. No attack roll has advantage against you while you aren’t incapacitated."
+            ],
           }
         ],
         class_specific: {
@@ -484,15 +514,17 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534da",
+      { _id: "rogueLevel19",
         level: 19,
         ability_score_bonuses: 5,
         prof_bonus: 6,
         feature_choices: [],
         features: [
           {
-            name: "Ability Score Improvement 6"
+            name: "Ability Score Improvement 6",
+            def: [
+              "When you reach 4th level, and again at 8th, 10th, 12th, 16th, and 19th level, you can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature."
+            ],
           }
         ],
         class_specific: {
@@ -506,15 +538,18 @@
           name: "Rogue"
         }
       },
-      {
-        _id: "58f40badc9e7ce9f721534db",
+      { _id: "rogueLevel20",
         level: 20,
         ability_score_bonuses: 5,
         prof_bonus: 6,
         feature_choices: [],
         features: [
           {
-            name: "Stroke of Luck"
+            name: "Stroke of Luck",
+            def: [
+              "At 20th level, you have an uncanny knack for succeeding when you need to. If your attack misses a target within range, you can turn the miss into a hit. Alternatively, if you fail an ability check, you can treat the d20 roll as a 20.",
+              "Once you use this feature, you can't use it again until you finish a short or long rest."
+            ],
           }
         ],
         class_specific: {
