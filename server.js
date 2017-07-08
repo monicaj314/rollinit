b303cc3492c7e2e178cfe97bb334ed6ba8a7d498
Monica Jones <monicaj314@yahoo.com>
2017-07-08 11:17:02 -0500

    mongo prod

--- server.js
+++ server.js
@@ -88,15 +88,15 @@ require("./routes/html-routes.js")(app);
 
 // =========  Database configuration with mongoose ===============
 var localMongo = "mongoose.connect('mongodb://localhost/rollinit')";
-//var MONGODB_URI = "mongodb://<dbuser>:<dbpassword>@ds125481.mlab.com:25481/heroku_0p3792pt"
+var MONGODB_URI = "mongodb://<dbuser>:<dbpassword>@ds125481.mlab.com:25481/heroku_0p3792pt"
 
-//if (process.env.MONGODB_URI){
+if (process.env.MONGODB_URI){
     // this executes if this is being executed in heroku app
-    //mongoose.connect(process.env.MONGODB_URI);
-//} else {
-    // this ececutes if this is being executed on local machine
+    mongoose.connect(process.env.MONGODB_URI);
+} else {
+   /// this executes if this is being executed on local machine
     mongoose.connect(localMongo);
-//}
+}
 
 
 
