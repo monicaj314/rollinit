d057dcd2d429bf06a0d20aca6ffb467b29a7115b
Monica Jones <monicaj314@yahoo.com>
2017-07-08 12:46:51 -0500

    cleanup

--- routes/users.js
+++ routes/users.js
@@ -88,22 +88,6 @@ passport.use(new LocalStrategy(
   }
 ));
 
-// ============ This is the basic from Passport ============
-// passport.use(new LocalStrategy(
-//   function(username, password, done) {
-//     User.findOne({ username: username }, function(err, user) {
-//       if (err) { return done(err); }
-//       if (!user) {
-//         return done(null, false, { message: 'Incorrect username.' });
-//       }
-//       if (!user.validPassword(password)) {
-//         return done(null, false, { message: 'Incorrect password.' });
-//       }
-//       return done(null, user);
-//     });
-//   }
-// ));
-
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
@@ -116,14 +100,9 @@ passport.deserializeUser(function(id, done) {
 
 
 router.post('/login',
-  passport.authenticate('local', {successRedirect: '/profile', failureRedirect: '/login',failureFlash: true}),
+  passport.authenticate('local', {successRedirect: '/profile', failureRedirect: '/',failureFlash: true}),
   function(req, res) {
-    // console.log("router.post/login")
-    
-    // localStorage.setItem('currentUser', req.user.username);
-    // var currentUser = localStorage.getItem('currentUser');
-    // console.log("localStorage username");
-    // console.log(currentUser);
+
 
     res.redirect('/profile');
   });
